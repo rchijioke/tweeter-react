c595974018033304ca4733cb739fa89ecf46380f
Roy <ekeneroy@gmail.com>
2023-06-29 19:29:18 -0500

    React state refactoring our state

---
+++ react-version/fancy-buttons/src/App.js
@@ -0,0 +1,24 @@
+import React, {useState} from 'react'
+import './App.css';
+import AngryButton from './components/AngryButton'
+import CounterButton from './components/CounterButton'
+import LightSwitchButton from './components/LightSwitchButton'
+import TextRepeaterButton from './components/TextRepeaterButton'
+
+function App() {
+  const [light, setLight] = useState('off');
+  const dark = (light === 'off') ? 'dark' : '';
+  return (
+    <div className={`App ${dark}`}>
+    <h1>Fancy Buttons!</h1>
+    <section>
+      <AngryButton />
+      <CounterButton />
+      <LightSwitchButton light={light} setLight={setLight} />
+      <TextRepeaterButton />
+    </section>
+  </div>
+  );
+}
+
+export default App;
---
+++ react-version/fancy-buttons/src/components/CounterButton.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+function CounterButton(){
+  const [clickAmount, setClickAmount] = useState(0);
+  const handleClick = () => setClickAmount(clickAmount + 1)
+  
+  return (
+    <button onClick={handleClick} className="CounterButton">
+      You clicked me {clickAmount} times
+    </button>
+  );
+}
+
+export default CounterButton;
